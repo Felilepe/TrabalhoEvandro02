b162bcf05d78c2ba01cdbb33c44adec6516e1f67
Felilepe <felipesepulveda536@gmail.com>
2025-12-05 01:27:37 -0300

    Feat: Adicionadas primeiras funções do módulo Polígono

--- src/poligono.c
+++ src/poligono.c
@@ -1,3 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "poligono.h"
+#include "lista.h"
+#include <stdbool.h>
+#include "linha.h"
+
+typedef struct stPonto //struct auxiliar para representar pontos
+{
+    double x;
+    double y;
+    bool isLast; // Indica se é o último ponto inserido   
+} ponto;
+
+
+typedef struct stPoligono 
+{
+    Lista* vertices; // Lista de pontos (vértices)
+    Lista* segmentos; // Lista de segmentos (arestas)
+    double xMin, yMin, xMax, yMax; // Bounding Box
+} poligono;
+
+Poligono poligono_create() 
+{
+    poligono* p = (poligono*)malloc(sizeof(poligono));
+    if (p == NULL) {
+        printf("Erro na alocacao de memoria ao criar poligono.");
+        exit(1);
+    }
+
+    p->vertices = lista_create();
+    p->segmentos = lista_create();
+    p->xMin = p->yMin = p->xMax = p->yMax = 0.0;
+
+    return (Poligono)p;
+}
+
+void poligono_insertPoint(Poligono p, double xp, double yp)
+{
+    poligono* poly = (poligono*)p;
+
+    ponto* novoPonto = (ponto*)malloc(sizeof(ponto));
+    if (novoPonto == NULL) {
+        printf("Erro na alocacao de memoria ao criar ponto.");
+        exit(1);
+    }
+    novoPonto->x = xp;
+    novoPonto->y = yp;
+    novoPonto->isLast = false;
+
+    lista_insere_fim(poly->vertices, (item)novoPonto);
+}
+
--- src/poligono.h
+++ src/poligono.h
@@ -1,9 +1,23 @@
 #ifndef POLIGONO_H
 #define POLIGONO_H
 
+#include "lista.h"
+#include <stdbool.h>
+
+
 typedef void* Poligono;
 
-Poligono poligono_create(int id);
+Poligono poligono_create();
+
+void poligono_insertPoint(Poligono p, double xp, double yp);
+
+void poligono_getBoundingBox(Poligono p, double *xMin, double *yMin, double *xMax, double *yMax);
+
+bool pligono_isInside(Poligono p, double xp, double yp);
+
+Lista *poligono_getVertices(Poligono p);
+
+Lista *poligono_getSegments(Poligono p);
 
 
 
