4b164f4963b1de2026bcddae5ce3ef3e190b8fff
Felilepe <felipesepulveda536@gmail.com>
2025-12-09 23:12:45 -0300

    Feat: Protótipos de funções do módulo Anteparo adicionados

---
+++ src/anteparo.h
@@ -0,0 +1,39 @@
+#ifndef ANTEPARO_H
+#define ANTEPARO_H
+
+typedef void *Anteparo;
+
+Anteparo antemparo_create(int id, double x1, double y1, double x2, double y2, const char *cor);
+
+
+int anteparo_getId(Anteparo a);
+
+double anteparo_getX1(Anteparo a);
+
+double anteparo_getY1(Anteparo a); 
+
+double anteparo_getX2(Anteparo a);
+
+double anteparo_getY2(Anteparo a);
+
+char *anteparo_getCor(Anteparo a);
+
+
+
+void anteparo_setId(Anteparo a, int novo_id);
+
+void anteparo_setX1(Anteparo a, double x1);
+
+void anteparo_setY1(Anteparo a, double y1);
+
+void anteparo_setX2(Anteparo a, double x2);
+
+void anteparo_setY2(Anteparo a, double y2);
+
+void anteparo_setCor(Anteparo a, const char *cor);
+
+
+
+void anteparo_destroy(Anteparo a);
+
+#endif
