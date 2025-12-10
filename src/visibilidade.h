9e1e7ec5e2c7dde2c9c78f8ef5dfa5334b8bb446
Felilepe <felipesepulveda536@gmail.com>
2025-12-10 10:33:46 -0300

    Feat: Adicionado módulo Visibilidade e prótotipos de função

---
+++ src/visibilidade.h
@@ -0,0 +1,21 @@
+#ifndef VISIBILIDADE_H
+#define VISIBILIDADE_H
+
+typedef void* evento;
+
+#include "anteparo.h"
+#include "lista.h"
+#include "arvore.h"
+#include "poligono.h"
+
+evento visibilidade_prepare(double x_bomba, double y_bomba, lista *anteparos, int *num_eventos);
+
+double visibilidade_getXRay(Arvore *segmentos, double x_origem, double y_origem, double angulo, double max_dist);
+
+double visibilidade_getYRay(Arvore *segmentos, double x_origem, double y_origem, double angulo, double max_dist);
+
+Poligono visibilidade_calc(double x_bomba, double y_bomba, lista *anteparos, char tipo_ord, double max_dist, int insert_limit);
+
+
+
+#endif
