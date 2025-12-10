27186d7b497ce9a3ab039e474e5031fdb7c59a53
Felilepe <felipesepulveda536@gmail.com>
2025-12-10 02:51:42 -0300

    Feat: Adicionadas novas funções no módulo Texto, referente aos anteparos e conversões

--- src/texto.h
+++ src/texto.h
@@ -1,5 +1,6 @@
 #ifndef TEXTO_H
 #define TEXTO_H
+#include "anteparo.h"
 
 typedef void* Texto;
 
@@ -187,5 +188,25 @@ void texto_setAnchor(Texto t, char a);
 *@param t O texto a ser destruído.
 ******************************************************************/
 void texto_destroy(Texto t);
+
+
+/******************************************************************
+*@brief Cria uma linha utilizando as coordenadas de um texto.
+*@param t O texto original.
+*@return A linha resultante.
+
+******************************************************************/
+Linha conversaoTxtoLinha(Texto t);
+
+
+
+/****************************************************************************************
+* @brief Converte um texto em um anteparo de acordo com as convenções do projeto.
+* @param t O texto a ser convertido.
+* @param ant_id Ponteiro para o ID do anteparo.
+* @return Retorna o anteparo criado.
+****************************************************************************************/
+Anteparo texto_anteparo(Texto t, int *ant_id);
+
 
 #endif
