1abc9c3fa2ca6e44fba1206a3871e68136b2f93b
Felilepe <felipesepulveda536@gmail.com>
2025-12-17 11:04:59 -0300

    feat: adiciona protótipo de TAD visibilidade

---
+++ src/visibilidade.h
@@ -0,0 +1,16 @@
+#ifndef VISIBILIDADE_H
+#define VISIBILIDADE_H
+#include "pontos.h"
+#include "arvore.h"
+#include "lista.h"
+#include "poligono.h"
+
+typedef struct stEvent Evento;
+
+Evento visibilidade_prepSegments(Lista *anteparos, Ponto bomb, int *event_count);
+
+Ponto visibilidade_rayCollision(Arvore *active_seg, Ponto bomb, double angle, double max_radius);
+
+Poligono visibilidade_calc(Lista *anteparos, Ponto bomb, char sort_type, double max_radius, int max_inserts);
+
+#endif
