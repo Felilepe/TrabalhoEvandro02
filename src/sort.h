53a489da9830e97105f8ec41c81360b4bdf7f0e2
Felilepe <felipesepulveda536@gmail.com>
2025-12-17 01:06:19 -0300

    feat: implementa TAD de algoritmos de ordenação

---
+++ src/sort.h
@@ -0,0 +1,17 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stddef.h>
+
+#ifndef ITEM_T_DEFINED
+typedef void *item;
+#define ITEM_T_DEFINED
+#endif
+
+int sort_doubleCompare(const item a, const item b);
+
+void insertionSort(item base, size_t element_count, size_t element_size, int (*sort_compare)(const item a, const item b));
+
+void mergeSort(item base, size_t element_count, size_t element_size, int (*sort_compare)(const item a, const item b), int threshold);
+
+
+#endif
